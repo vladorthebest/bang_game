ca4eecbbc96b0b86d6f1120ec5fdebbe8cbd35d3
Vladyslav Nosov <vladorthebest@gmail.com>
2023-03-21 14:04:24 +0100

    Create BaseBrownCard

---
+++ src/main/java/sk/stuba/fei/uim/oop/game/bang/cards/BaseBrownCard.java
@@ -0,0 +1,16 @@
+package sk.stuba.fei.uim.oop.game.bang.cards;
+
+import sk.stuba.fei.uim.oop.game.bang.player.Player;
+
+public class BaseBrownCard extends BaseCard {
+
+    @Override
+    public void use(Player usingPlayer, Player targetPlayer) {
+        return;
+    }
+
+    private void discardCard(Player owner){
+
+    }
+
+}
