429ae1bb8d41548d49a4d6143a706f1e2e5ba975
Vladyslav Nosov <vladorthebest@gmail.com>
2023-03-21 17:09:01 +0100

    Reconstruction struct ActiveCards

--- src/main/java/sk/stuba/fei/uim/oop/game/bang/cards/MissedCard.java
+++ src/main/java/sk/stuba/fei/uim/oop/game/bang/cards/MissedCard.java
@@ -2,7 +2,7 @@ package sk.stuba.fei.uim.oop.game.bang.cards;
 
 import sk.stuba.fei.uim.oop.game.bang.player.Player;
 
-public class MissedCard extends BasePassiveCard{
+public class MissedCard extends PassiveCard{
     {
         this.name = "Missed";
         this.type = Color.BROWN;
