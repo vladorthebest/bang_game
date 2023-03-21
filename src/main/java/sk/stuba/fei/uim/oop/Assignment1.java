4cf752db6b98d427994e063ee914e2255db83b91
Vladyslav Nosov <vladorthebest@gmail.com>
2023-03-21 14:07:24 +0100

    Add start() in main()

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -1,7 +1,10 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.game.bang.Bang;
+
 public class Assignment1 {
     public static void main(String[] args) {
-        
+        Bang game = new Bang();
+        game.start();
     }
 }
