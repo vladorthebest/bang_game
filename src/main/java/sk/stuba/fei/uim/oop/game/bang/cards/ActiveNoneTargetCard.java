eb0137ca27558c8a7bb0dc91516ea2f2ebd3bc64
Vladyslav Nosov <vladorthebest@gmail.com>
2023-03-21 20:23:03 +0100

    Refactoring

--- src/main/java/sk/stuba/fei/uim/oop/game/bang/cards/ActiveNoneTargetCard.java
+++
@@ -1,7 +0,0 @@
-package sk.stuba.fei.uim.oop.game.bang.cards;
-
-import sk.stuba.fei.uim.oop.game.bang.player.Player;
-
-public abstract class ActiveNoneTargetCard extends BaseCard{
-    public void use(Player usingPlayer) {};
-}
