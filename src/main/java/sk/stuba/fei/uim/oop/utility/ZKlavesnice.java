31031f22c645ff34fd4df69578b147891da4131f
Vladyslav Nosov <vladorthebest@gmail.com>
2023-03-23 23:09:14 +0100

    Translate ZKlavesnice on eng

--- src/main/java/sk/stuba/fei/uim/oop/utility/ZKlavesnice.java
+++ src/main/java/sk/stuba/fei/uim/oop/utility/ZKlavesnice.java
@@ -13,7 +13,7 @@ public class ZKlavesnice {
             c = (char) zklavesnice.read();
             //System.out.println("Nacital som " + c);
         } catch (Exception e) {
-            System.out.println("nepodarilo sa");
+            System.out.println("Invalid input");
             c = readChar(napis_pre_uzivatela);
         }
 
@@ -28,7 +28,7 @@ public class ZKlavesnice {
             s = zklavesnice.readLine();
             //System.out.println("Nacital som " + s);
         } catch (Exception e) {
-            System.out.println("nepodarilo sa");
+            System.out.println("Invalid input");
             s = readString(napis_pre_uzivatela);
         }
 
@@ -45,7 +45,7 @@ public class ZKlavesnice {
             //System.out.println("Nacital som " + s);
             n = Integer.parseInt(s);
         } catch (Exception e) {
-            System.out.println("nepodarilo sa");
+            System.out.println("Invalid input");
             n = readInt(napis_pre_uzivatela);
         }
 
@@ -62,7 +62,7 @@ public class ZKlavesnice {
             //System.out.println("Nacital som " + s);
             x = Double.parseDouble(s);
         } catch (Exception e) {
-            System.out.println("nepodarilo sa");
+            System.out.println("Invalid input");
             x = readDouble(napis_pre_uzivatela);
         }
 
